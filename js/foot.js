5a613993ddb5ebcd566e7fa542ecc42ced9c89b8
sxr1023 <sun15252223914@outlook.com>
2026-06-23 18:16:33 +0800

    Site updated: 2026-06-23 18:16:33

--- js/foot.js
+++ js/foot.js
@@ -1,24 +1,3 @@
-// 页脚养鱼
-fish();
-function fish() {
-  return (
-    $("#footer-wrap").css({
-      position: "absolute",
-      "text-align": "center",
-      top: -55,
-      right: 0,
-      left: 0,
-      bottom: 0,
-    }),
-    $("footer").append(
-      '<div class="container" id="jsi-flying-fish-container"></div>'
-    ),
-    $("body").append(
-      '<script src="https://fastly.jsdelivr.net/gh/xiabo2/CDN@latest/fish.js"></script>'
-    ),
-    this
-  );
-}
 
 // 动态心跳，更改自己的名称
 $(document).ready(function (e) {
