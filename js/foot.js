b8bab9456c318f92a4348e7bdbe1e8093a06667b
sxr1023 <sun15252223914@outlook.com>
2026-06-25 16:07:52 +0800

    Site updated: 2026-06-25 16:07:51

--- js/foot.js
+++ js/foot.js
@@ -48,11 +48,11 @@ var titleTime;
 document.addEventListener("visibilitychange", function () {
   if (document.hidden) {
     //离开当前页面时标签显示内容
-    document.title = "👀跑哪里去了~";
+    document.title = "(｡>﹏<｡) 别走呀~";
     clearTimeout(titleTime);
   } else {
     //返回当前页面时标签显示内容
-    document.title = "🐖抓到你啦～";
+    document.title = "(≧∇≦)ﾉ欢迎回来！";
     //两秒后变回正常标题
     titleTime = setTimeout(function () {
       document.title = OriginTitile;
