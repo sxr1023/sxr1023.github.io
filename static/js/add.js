0ee47f2fe68554fc2a85fc5dfd9b3612fcda2257
sxr1023 <sun15252223914@outlook.com>
2026-07-18 09:48:04 +0000

    Site updated: 2026-07-18 17:47:21 By hexo-pro Updated By Github Actions

--- static/js/add.js
+++ static/js/add.js
@@ -1,12 +1,17 @@
 function setTagClass(){
-  document.body.classList.remove('is-tags', 'is-categories', 'is-posts')
+  document.body.classList.remove('is-tags', 'is-categories', 'is-posts', 'is-banner_img')
   let path = location.pathname
   // 匹配 /tags/ 以及分页 /tags/page/1/ 等
   if(path.startsWith('/tags/')){
     document.body.classList.add('is-tags')
+    document.body.classList.add('is-banner_img')
   }
   if(path.startsWith('/categories/')){
     document.body.classList.add('is-categories')
+    document.body.classList.add('is-banner_img')
+  }
+  if(path.startsWith('/essay/')){
+    document.body.classList.add('is-banner_img')
   }
   if(path.startsWith('/posts/')){
     document.body.classList.add('is-posts')
